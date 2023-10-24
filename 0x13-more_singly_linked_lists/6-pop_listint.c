378ad521d1bd00c26f15b865b4765f831d50aa24
Dr-Princess <yangasobekwa208@gmail.com>
2023-10-23 17:46:50 -0700

    task 6

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -14,11 +14,11 @@ int pop_listint(listint_t **head)
 
 	if (!head || !*head)
 		return (0);
-
+	{
 		num = (*head)->n;
 		temp = (*head)->next;
 		free(*head);
 		*head = temp;
-
+	}
 	return (num);
 }
